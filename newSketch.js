fbcde719893b3e0e603604227a6cde3bdb8dd377
Goldenfoxy523 <goldenfoxy523@gmail.com>
2020-12-05 17:45:00 -0500

    Added a smoke image

---
+++ newSketch.js
@@ -0,0 +1,34 @@
+var x = 10;
+
+var tempArray = [];
+
+var tempArray = [10,20,30,40,50];
+
+//console.log(tempArray.length);
+
+//console.log(tempArray[0]);
+
+//console.log(tempArray[tempArray.length - 1]);
+
+tempArray.push(60);
+
+for(var i = 0; i< tempArray.length; i++){
+   // console.log(tempArray[i])
+}
+
+//tempArray.pop();
+
+console.log("After removing the last element")
+
+for(var i = 0; i< tempArray.length; i++){
+    //console.log(tempArray[i])
+}
+
+
+var temp2 = [[10,100], [20,200], [30,300], [40,400]]
+
+console.log(temp2[0]);
+
+console.log(temp2[0][0]);
+
+console.log(temp2[2][1]);
